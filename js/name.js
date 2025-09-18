aced1e96414583012fa260bab9f9d7cf7555f5ca
benjaa <1670479@senati.pe>
2025-09-18 17:56:23 -0500

    Add files via upload

---
+++ js/name.js
@@ -0,0 +1,25 @@
+nombre=prompt("escriba su nombre");
+let num1,num2,num3,pros;
+num1=parseInt(prompt("ingrese primera nota: "));
+num2=parseInt(prompt("ingrese segunda nota: "));
+num3=parseInt(prompt("ingrese tercera nota: "));
+prom=Math.round((num1+num2+num3)/3);
+Math.floor.round((num1+num2+num3)/3);
+document.writeln("Binvenido "+nombre)
+document.writeln(" <br> ");
+document.writeln("sus notas son="+" n1:"+ num1 +" n2"+ num2 +" n3 "+num3);
+document.writeln(" <br> ");
+document.writeln("el promedio es "+prom)
+document.writeln(" <br> ");
+document.writeln("promedio sin redondear: "+prom1);
+document.writeln(" <br> ");
+let mensaje=prompt('escriba una frase o mensaje : ')
+
+document.writeln(" <br> ");
+document.writeln(texto,length);
+document.writeln(" <br> ");
+document.writeln(texto.tdUpperCase());
+document.writeln(" <br> ");
+document.writeln(texto.split("fraces"));
+document.writeln(" <br> ");
+document.writeln(texto.replace("texto es", "informacion importante"));
